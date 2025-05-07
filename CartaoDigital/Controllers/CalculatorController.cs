23d37556f6c028b9f1f4865c9e3ced79cb991a31
Lucas Tomaz <olucastomaz@hotmail.com>
2025-05-06 22:07:15 -0300

    first commit

---
+++ CartaoDigital/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CartaoDigital.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public int GetSoma(int num1, int num2)
+        {
+            var result = num1 + num2;
+            return result;
+        }
+
+        [HttpGet("Multiplicacao")]
+        public int GetMultiplicacao(int num1, int num2)
+        {
+            var result = num1 * num2;
+            return result;
+        }
+
+        [HttpPost]
+        public double GetDivisao([FromBody] DivisaoRequest request)
+        {
+            return request.Divisao();
+        }
+
+        public class DivisaoRequest
+        {
+            public double Num1 { get; set; }
+            public double Num2 { get; set; }
+            public double Divisao()
+            {
+                return Num1 / Num2;
+            }
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# 💳 Digital Card API
+
+API desenvolvida durante o curso Backend .NET com C# (FIAP).  
+Objetivo: fornecer o backend de um aplicativo de cartão digital.
+
+## 🚀 Funcionalidades
+
+- [ ] Cadastro de clientes
+- [ ] Geração de novos cartões
+- [ ] Realização de transações
+- [ ] Consulta de extrato
+
+## 🧪 Tecnologias utilizadas
+
+- C# com .NET Core
+- ASP.NET Web API
+- Entity Framework
+- SQL Server
